b7ce18fa459115e680ef3d927e4010b6eaff57cb
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-11-12 10:13:32 +0530

    depedency install & env.sample created

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get('/health', (req, res) => {
+    res.send('Server is healthy');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on  http://localhost:${PORT}`);
+});
